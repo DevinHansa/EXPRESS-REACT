05f131217ff2c1bba25f60392e8232e467cb3b64
DevinHansa <devinhansa@gmail.com>
2023-12-21 21:01:34 +0530

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-
-  const [youtubeCTA, setYoutubeCTA] = useStae({});
+  const [youtubeCTA, setYoutubeCTA] = useState({});
 
   useEffect(() => {
-
     const fetchData = async () => {
       try {
         const response = await fetch("http://localhost:8000/api/youtube");
 
         if (!response.ok) {
-          throw new Error('HTTP error! Status:${response.status}');
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const data = await response.json();
         console.log(data);
 
       } catch (error) {
-          console.log("Error etching data :{error.message}")
+        console.log(`Error fetching data: ${error.message}`);
       }
-    }
+    };
 
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <div>App</div>
-  )
+  );
 }
 
-export default App
+export default App;
